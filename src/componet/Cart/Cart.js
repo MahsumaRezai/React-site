2e018db98fd8563655b3d1e23554df1136140d0c
Masoumeh Rezai <you@example.com>
2023-06-28 03:25:58 +0430

    add text

--- src/componet/Cart/Cart.js
+++ src/componet/Cart/Cart.js
@@ -1,11 +1,19 @@
 import classes from './Cart.module.css';
 import imgOne from '../image/service1.jpg'
-const Cart=(props)=>{
-    return(
-        <div className={classes.cart}>
-            <img  className={classes.img} src={imgOne} alt=''/>
+import { Fragment } from 'react';
+import Text from './Text'
+const Cart = (props) => {
+    return (
+        <Fragment>
 
-        </div>
+            <div className={classes.cart}>
+                <img className={classes.img} src={imgOne} alt='' />
+                <Text/>
+
+
+            </div>
+        </Fragment>
+
     )
 }
 export default Cart;
---
+++ src/componet/Cart/Text.js
@@ -0,0 +1,13 @@
+import classes from './Text.module.css'
+const Text = (props) => {
+    return (
+        <div>
+            <h2 className={classes.title}>Veniam, Quis</h2>
+            <p>sed do eiusmod tempor incididunt ut<br></br>
+                labore et dolore magna aliqua.
+                Ut enim<br></br> ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+
+        </div>
+    )
+}
+export default Text;
