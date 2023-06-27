fce20314298db0595ad429cc04d6528af312f016
Masoumeh Rezai <you@example.com>
2023-06-28 04:01:04 +0430

    add text

---
+++ src/componet/Button/Button.js
@@ -0,0 +1,10 @@
+const Button = (props) => {
+    return (
+        <div>
+            {props.children}
+            <button>Read</button>
+        </div>
+
+    )
+}
+export default Button;
--- src/componet/Cart/Cart.js
+++ src/componet/Cart/Cart.js
@@ -2,6 +2,7 @@ import classes from './Cart.module.css';
 import imgOne from '../image/service1.jpg'
 import { Fragment } from 'react';
 import Text from './Text'
+import Button from '../Button/Button';
 const Cart = (props) => {
     return (
         <Fragment>
@@ -9,6 +10,7 @@ const Cart = (props) => {
             <div className={classes.cart}>
                 <img className={classes.img} src={imgOne} alt='' />
                 <Text/>
+                <Button/>
 
 
             </div>
