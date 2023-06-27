9d16d94b1305b9b6169888e116e20e5e6be27a32
Masoumeh Rezai <you@example.com>
2023-06-28 02:05:55 +0430

    cart style

--- src/componet/Section/SectionOne.js
+++ src/componet/Section/SectionOne.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react"
 import SectionHead from "./SectionHead";
 import classes from './SectionOne.module.css'
+import Cart from "../Cart/Cart";
 
 const SectionOne = (props) => {
     return (
         <Fragment>
             <main className={classes.main}>
                 <SectionHead />
+                <Cart/>
 
             </main>
 
