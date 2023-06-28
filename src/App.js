46e44a32de65959173ea390a3eb4c0be072acff7
Masoumeh Rezai <you@example.com>
2023-06-28 10:09:36 +0430

    add usetlist

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 import Header from "./componet/UI/Header";
 import SectionOne from "./componet/Section/SectionOne";
 import AddUser from "./componet/AddUser/AddUser";
+import UserList from "./componet/CheckFrom/UserList";
 function App() {
   return (
     <Fragment>
       <Header />
       <SectionOne/>
       <AddUser/>
+      <UserList/>
       
     </Fragment>
 
---
+++ src/componet/CheckFrom/UserList.js
@@ -0,0 +1,14 @@
+import Cartform from "../AddUser/Cartfrom";
+
+const UserList = (props) => {
+    return (
+        <div>
+            <Cartform>
+
+            </Cartform>
+
+        </div>
+    )
+
+}
+export default UserList;
