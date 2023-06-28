1f20ac5f56b5fc18736e3a58b75ab0585b4349ba
Masoumeh Rezai <you@example.com>
2023-06-28 07:07:39 +0430

    work on add information form

--- src/componet/AddUser/AddUser.js
+++ src/componet/AddUser/AddUser.js
@@ -1,11 +1,34 @@
 import classes from './Add.module.css'
 import Button from './Button';
 import Cartform from './Cartfrom';
+import { useState } from 'react';
 const AddUser = (posp) => {
+    const [nameEnter, setEnter] = useState('');
+    const [lastnameEnter, setlastEnter] = useState('');
+    const [emailEnter, setemailEnter] = useState('');
+    const [passwordEnter, setpasswordEnter] = useState('');
+
+    const formHandler = (event) => {
+        event.preventDefault();
+
+    }
+    const nameInputHandler = (event) => {
+        setEnter(event.traget.value);
+    }
+
+    const lastInputHandler = (event) => {
+        setlastEnter(event.traget.value);
+    }
+    const emailInputHandler = (event) => {
+        setemailEnter(event.traget.value);
+    }
+    const passwordInputHandler = (event) => {
+        setpasswordEnter(event.traget.value);
+    }
     return (
         <Cartform>
 
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={formHandler}>
                 <label htmlFor="name">Name</label>
                 <input id="name" className={classes.input}></input>
                 <label htmlFor="lastname">LastName</label>
